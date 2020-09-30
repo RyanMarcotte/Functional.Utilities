a6bb07a175d90053edf3895308570bbc406f6574
Justin Cooney <justin.d.cooney@gmail.com>
2020-09-30 14:32:42 -0600

    Adds extension methods for Task wrapped Result and Option type assertions.

---
+++ src/Functional.Primitives.FluentAssertions/Functional.Primitives.FluentAssertions.Tests/OptionTypeAssertionsExtensionsTests.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Functional.Primitives.FluentAssertions.Tests
+{
+	public class OptionTypeAssertionsExtensionsTests
+	{
+		public class ValueChecks
+		{
+			private const int VALUE = 3;
+
+			[Fact]
+			public void ShouldNotThrowException() => new Func<Task>(() =>
+				Task.FromResult(Option.Some(VALUE))
+					.Should()
+					.HaveValue())
+			.Should().NotThrow();
+
+			[Fact]
+			public void ShouldNotThrowExceptionWhenAdditionalAssertionSucceeds() => new Func<Task>(() =>
+				Task.FromResult(Option.Some(VALUE))
+					.Should()
+					.HaveValue()
+					.AndValue(value => value.Should().Be(VALUE)))
+			.Should().NotThrow();
+
+			[Fact]
+			public void ShouldThrowException() => new Func<Task>(() =>
+				Task.FromResult(Option.None<int>())
+					.Should()
+					.HaveValue())
+			.Should().Throw<Exception>();
+
+			[Fact]
+			public void ShouldThrowExceptionWhenAdditionalAssertionFails() => new Func<Task>(() =>
+				Task.FromResult(Option.Some(VALUE))
+					.Should()
+					.HaveValue()
+					.AndValue(value => value.Should().Be(VALUE)))
+			.Should().NotThrow();
+		}
+
+		public class NoValueChecks
+		{
+			[Fact]
+			public void ShouldNotThrowException() => new Func<Task>(() => Task.FromResult(Option.None<int>()).Should().NotHaveValue()).Should().NotThrow();
+
+			[Fact]
+			public void ShouldThrowException() => new Func<Task>(() => Task.FromResult(Option.Some(3)).Should().NotHaveValue()).Should().Throw<Exception>();
+		}
+
+		public class IntegerOptionChecks
+		{
+			private const int VALUE = 54;
+
+			[Fact]
+			public void ShouldNotThrowExceptionWhenBothSomeAndSameValues() => new Func<Task>(() =>
+				Task.FromResult(Option.Some(VALUE))
+					.Should()
+					.Be(Option.Some(VALUE)))
+			.Should().NotThrow();
+
+			[Fact]
+			public void ShouldNotThrowExceptionWhenBothNone() => new Func<Task>(() =>
+				Task.FromResult(Option.None<int>())
+					.Should()
+					.Be(Option.None<int>()))
+			.Should().NotThrow();
+
+			[Fact]
+			public void ShouldThrowExceptionWhenBothSomeButDifferentValues() => new Func<Task>(() =>
+				Task.FromResult(Option.Some(VALUE))
+					.Should()
+					.Be(Option.Some(VALUE+1)))
+			.Should().Throw<Exception>();
+
+			[Fact]
+			public void ShouldThrowExceptionWhenLeftIsSomeButRightIsNone() => new Func<Task>(() =>
+				Task.FromResult(Option.Some(VALUE))
+					.Should()
+					.Be(Option.None<int>()))
+			.Should().Throw<Exception>();
+
+			[Fact]
+			public void ShouldThrowExceptionWhenLeftIsNoneButRightIsSome() => new Func<Task>(() =>
+				Task.FromResult(Option.None<int>())
+					.Should()
+					.Be(Option.Some(VALUE)))
+			.Should().Throw<Exception>();
+		}
+
+		public class StringOptionChecks
+		{
+			private const string VALUE = "test";
+
+			[Fact]
+			public void ShouldNotThrowExceptionWhenBothSomeAndSameValues() => new Func<Task>(() =>
+				Task.FromResult(Option.Some(VALUE))
+					.Should()
+					.Be(Option.Some(VALUE)))
+			.Should().NotThrow();
+
+			[Fact]
+			public void ShouldNotThrowExceptionWhenBothNone() => new Func<Task>(() =>
+				Task.FromResult(Option.None<string>())
+					.Should()
+					.Be(Option.None<string>()))
+			.Should().NotThrow();
+
+			[Fact]
+			public void ShouldThrowExceptionWhenBothSomeButDifferentValues() => new Func<Task>(() =>
+				Task.FromResult(Option.Some(VALUE))
+					.Should()
+					.Be(Option.Some(VALUE + 1)))
+			.Should().Throw<Exception>();
+
+			[Fact]
+			public void ShouldThrowExceptionWhenLeftIsSomeButRightIsNone() => new Func<Task>(() =>
+				Task.FromResult(Option.Some(VALUE))
+					.Should()
+					.Be(Option.None<string>()))
+			.Should().Throw<Exception>();
+
+			[Fact]
+			public void ShouldThrowExceptionWhenLeftIsNoneButRightIsSome() => new Func<Task>(() =>
+				Task.FromResult(Option.None<string>())
+					.Should()
+					.Be(Option.Some(VALUE)))
+			.Should().Throw<Exception>();
+		}
+
+		public class EquatableClassOptionChecks
+		{
+			private const int VALUE = 1337;
+
+			[Fact]
+			public void ShouldNotThrowExceptionWhenBothSomeAndSameValues() => new Func<Task>(() =>
+				Task.FromResult(Option.Some(new EquatableClass(VALUE)))
+					.Should()
+					.Be(Option.Some(new EquatableClass(VALUE))))
+			.Should().NotThrow();
+
+			[Fact]
+			public void ShouldNotThrowExceptionWhenBothNone() => new Func<Task>(() =>
+				Task.FromResult(Option.None<string>())
+					.Should()
+					.Be(Option.None<string>()))
+			.Should().NotThrow();
+
+			[Fact]
+			public void ShouldThrowExceptionWhenBothSomeButDifferentValues() => new Func<Task>(() =>
+				Task.FromResult(Option.Some(new EquatableClass(VALUE)))
+					.Should()
+					.Be(Option.Some(new EquatableClass(VALUE + 1))))
+			.Should().Throw<Exception>();
+
+			[Fact]
+			public void ShouldThrowExceptionWhenLeftIsSomeButRightIsNone() => new Func<Task>(() =>
+				Task.FromResult(Option.Some(new EquatableClass(VALUE)))
+					.Should()
+					.Be(Option.None<EquatableClass>()))
+			.Should().Throw<Exception>();
+
+			[Fact]
+			public void ShouldThrowExceptionWhenLeftIsNoneButRightIsSome() => new Func<Task>(() =>
+				Task.FromResult(Option.None<EquatableClass>())
+					.Should()
+					.Be(Option.Some(new EquatableClass(VALUE))))
+			.Should().Throw<Exception>();
+
+			#region Models
+
+			private class EquatableClass : IEquatable<EquatableClass>
+			{
+				public EquatableClass(int value)
+				{
+					Value = value;
+				}
+
+				public int Value { get; }
+
+				public bool Equals(EquatableClass other)
+				{
+					if (other is null) return false;
+					if (ReferenceEquals(this, other)) return true;
+					return Value == other.Value;
+				}
+
+				public override bool Equals(object obj)
+				{
+					if (obj is null) return false;
+					if (ReferenceEquals(this, obj)) return true;
+					return obj.GetType() == GetType() && Equals((EquatableClass)obj);
+				}
+
+				public override int GetHashCode() => Value;
+				public static bool operator ==(EquatableClass left, EquatableClass right) => Equals(left, right);
+				public static bool operator !=(EquatableClass left, EquatableClass right) => !Equals(left, right);
+			}
+
+			#endregion
+		}
+	}
+}
---
+++ src/Functional.Primitives.FluentAssertions/Functional.Primitives.FluentAssertions/OptionTypeAssertionsExtensions.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using FluentAssertions.Primitives;
+using System;
+using System.Threading.Tasks;
+
+namespace Functional.Primitives.FluentAssertions
+{
+	/// <summary>
+	/// A collection of extensions for <see cref="OptionTypeAssertions{T}"/>
+	/// </summary>
+	public static class OptionTypeAssertionsExtensions
+	{
+		/// <summary>
+		/// Verifies that the subject is equal to an expected value.
+		/// </summary>
+		/// <typeparam name="T">The contained type.</typeparam>
+		/// <param name="source">The source <see cref="OptionTypeAssertions{T}"/> task.</param>
+		/// <param name="expected">The expected value.</param>
+		/// <param name="because">Additional information for if the assertion fails.</param>
+		/// <param name="becauseArgs">Zero or more objects to format using the placeholders in <paramref name="because"/>.</param>
+		/// <returns></returns>
+		public static async Task<AndConstraint<ObjectAssertions>> Be<T>(this Task<OptionTypeAssertions<T>> source, Option<T> expected, string because = "", params object[] becauseArgs)
+			=> (await source).Be(expected, because, becauseArgs);
+
+		/// <summary>
+		/// Verifies that the subject <see cref="Option{T}"/> holds a value.
+		/// </summary>
+		/// <typeparam name="T">The contained type.</typeparam>
+		/// <param name="source">The source <see cref="OptionTypeAssertions{T}"/> task.</param>
+		/// <param name="because">Additional information for if the assertion fails.</param>
+		/// <param name="becauseArgs">Zero or more objects to format using the placeholders in <paramref name="because"/>.</param>
+		public static async Task<AndOptionValueConstraint<T>> HaveValue<T>(this Task<OptionTypeAssertions<T>> source, string because = "", params object[] becauseArgs)
+			=> (await source).HaveValue(because, becauseArgs);
+
+		/// <summary>
+		/// Verifies that the subject <see cref="Option{T}"/> does not hold a value.
+		/// </summary>
+		/// <typeparam name="T">The contained type.</typeparam>
+		/// <param name="source">The source <see cref="OptionTypeAssertions{T}"/> task.</param>
+		/// <param name="because">Additional information for if the assertion fails.</param>
+		/// <param name="becauseArgs">Zero or more objects to format using the placeholders in <paramref name="because"/>.</param>
+		/// <returns></returns>
+		public static async Task NotHaveValue<T>(this Task<OptionTypeAssertions<T>> source, string because = "", params object[] becauseArgs)
+			=> (await source).NotHaveValue();
+
+		/// <summary>
+		/// Invokes the provided action on the value of the option.
+		/// </summary>
+		/// <typeparam name="T">The contained type.</typeparam>
+		/// <param name="source">The <see cref="AndOptionValueConstraint{T}"/> with the possible value to perform additional assertion actions on.</param>
+		/// <param name="action">The assertion action to perform on the option's value.</param>
+		public static async Task AndValue<T>(this Task<AndOptionValueConstraint<T>> source, Action<T> action)
+			=> action((await source).AndValue);
+	}
+}
